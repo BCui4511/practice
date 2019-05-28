41f872c8852a7da52763ffb1400ff642867d74e8
BCui <zs_cui@foxmail.com>
2019-05-28 22:48:22 +0800

    fix:更改使用方式(未参考第三方代码)

---
+++ tab/index.js
@@ -0,0 +1,10 @@
+// opt 选项
+// opt = {
+//     startPage: Number,
+//     style: String,
+// }
+const opt = {
+    startPage: 1,
+}
+const tab1 = new Tab('tab1', opt);
+const tab2 = new Tab('tab2');
---
+++ tab/tab.js
@@ -0,0 +1,54 @@
+function Tab(id, opt) {
+    this.tabNode = document.getElementById(id);
+    this.liList = this.tabNode.firstElementChild.children;
+    this.pageList = this.tabNode.lastElementChild.children;
+    this.pageCount = this.pageList.length;
+    this.currentPage = -1;
+    this.init(opt);
+    this.tabNode.addEventListener('click',(e) => {
+        if (typeof e.target.dataset.tabIndex !=='string') {
+            return;
+        }
+        const index = Number(e.target.dataset.tabIndex);
+        this.changePage(index);
+    })
+}
+// 初始化
+Tab.prototype.init = function (opt) {
+    const tabClass = this.tabNode.classList;
+    if (!tabClass.contains('cover-tab-style')) {
+        if (opt && opt.style) {
+            tabClass.add(opt.style);
+        } else {
+            tabClass.add('normal-tab');
+        }
+    }
+    Array.from(this.pageList).forEach((item) => {
+        item.classList.add('tab-page');
+    });
+    Array.from(this.liList).forEach((item,index) => {
+        item.dataset.tabIndex = index;
+    });
+    const n = opt ? (opt.startPage || 0) : 0;
+    this.changePage(n);
+}
+// 切换页面
+Tab.prototype.changePage = function (n) {
+    if (this.currentPage === n) {
+        return;
+    }
+    if (this.currentPage > -1) {
+        this.liList[this.currentPage].classList.remove('clicked-li');
+        this.pageList[this.currentPage].classList.remove('clicked-page');
+    }
+
+    this.liList[n].classList.add('clicked-li');
+    this.pageList[n].classList.add('clicked-page');
+    this.currentPage = n;
+}
+
+// opt 选项
+// opt = {
+//     startPage: Number,
+//     style: String,
+// }
